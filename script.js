df932e5f8d972314d0ca503d2e1fb3975a0f27e3
Charlie Cuddy <charliecuddy4@gmail.com>
2018-02-15 06:37:13 +0000

    working division of the field

--- script.js
+++ script.js
@@ -1,101 +1,62 @@
-var day = 0;
+var day = 1;
 var youngerBrotherPercent = document.getElementById('youngerBrotherPercent');
 var olderBrotherPercent = document.getElementById('olderBrotherPercent');
 
-function nextPrimeClick() {
-  if (day < 365) {
-     youngerBrotherPercent.innerHTML =  /* primeNumbersArray[myIndex]; */
-     olderBrotherPercent.innerHTML = /* primeNumbersArray[myIndex - 1]; */
-     day++;
-  }
-  else {
-     youngerBrotherPercent.innerHTML = "That was a full year... the brother's are tired of dividing their field.";
-     olderBrotherPercent.innerHTML = "";
-  }
+var percentArray1 = [0.5]
+var percentArray2 = [0.5]
 
+function count() {
+    var value1 = 1/2;
+    var value2 = 1/2;  
+    for(i = 0; i < 42; i++) {
 
-var value1 = 1/2;
+        var value1 = value1 - value1/3;
 
-var value2 = 1/2;
+        var value2 = value2 + value1/2;
 
-function sunSet() {
-    for (var day = 0; day < 365; day++) {
-   
-    var value1 = value1 - value1/3;
+        var value2 = value2 - value2/3;
 
-    var value2 = value2 + value1/2;
+        var value1 = value1 + value2/2;
 
-    var value2 = value2 - value2/3;
-
-    var value1 = value1 + value2/2;
-
-    var value2 = 1 - value1; 
-    
-    console.log(value1);
-    
-    console.log(value2);
-    
+        var value2 = 1 - value1; 
         
-    }
+        percentArray1.push(value1);
+        percentArray2.push(value2);
 
+
+        console.log("younger: " + value1*100 + "%");
+
+        console.log("older: " + value2*100  + "%");
+
+        console.log(percentArray1);
+        console.log(percentArray2);
+    }
 }
+count();
 
+function nextDayClick() {
+    
+  if (day < 42) {
 
-
-
-
-/*
-
-// Identify Prime Numbers
-
-// If # is divisible by 2, 3, 5, 7 then not prime else prime
-// Run from 2 to infinity as array
-// Loop through prime array and check if each i is divisible
-
-
-
-// puts 1 to 8000 in an array
-
-    var allNumbersArray = [0, 1]
-    var primeNumbersArray = ['', 2, 3, 5, 7]
-    function findPrimes() {        
-        for (var i = 2; i < 8000; i++) {
-        allNumbersArray.push(i);
-            if (i % 2 !== 0 && i % 3 !== 0 && i % 5 !== 0 && i % 7 !== 0 && i % Math.sqrt(i) !==0) {
-            primeNumbersArray.push(i);  
-            }    
-        }
-    }
-    findPrimes();
-
-
-// if (primeNumbersArray[i] divides into i then do not put it in primeNumbersArray)
-
-var myIndex = 1;
-var currentPrimeNumber = document.getElementById('currentPrimeNumber');
-var previousPrimeNumber = document.getElementById('previousPrimeNumber');
-var futurePrimeNumber = document.getElementById('futurePrimeNumber');
-
-currentPrimeNumber.innerHTML = primeNumbersArray[0]; //Print first value of array right away.
-previousPrimeNumber.innerHTML = primeNumbersArray[0];
-futurePrimeNumber.innerHTML = primeNumbersArray[1];
-
-function nextPrimeClick() {
-  if (myIndex < primeNumbersArray.length) {
-     currentPrimeNumber.innerHTML = primeNumbersArray[myIndex];
-     previousPrimeNumber.innerHTML = primeNumbersArray[myIndex - 1];
-     futurePrimeNumber.innerHTML = primeNumbersArray[myIndex + 1];
-     myIndex++;
+     dayCount.innerHTML = day;
+     youngerBrotherPercent.innerHTML = percentArray1[day]*100;
+     olderBrotherPercent.innerHTML = percentArray2[day]*100;
+     console.log(day);
+     day++;
+     console.log(percentArray1);
+ 
   }
+    
   else {
-     currentPrimeNumber.innerHTML = "CONGRATS! You made it through first 1000 Prime Numbers!";   
+     dayCount.innerHTML = "42 - infinitly small exchange of land - no noticable increase or decrease."
+     youngerBrotherPercent.innerHTML = " 60";
+     olderBrotherPercent.innerHTML = " 40";
   }
-};
+
+}
 
 
-*/
-
-
+ 
 
 
 
@@ -103,14 +64,11 @@ function nextPrimeClick() {
 
 
 /*
-var myButton = document.getElementById("clickButton");
-var myText = document.getElementById("helloText");
 
-myButton.addEventListener('click', doSomething, false)
+Step by step through my thought process... 
 
-function doSomething() {
-	myText.textContent = "hello, world!";
-}
+
+
 var day = 0;
 var field = 1/2;
 var peter = field - (field/3);
